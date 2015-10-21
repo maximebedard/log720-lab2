e838d6d7beb24e9cfb9080c08c94371ed00eac36
Maxime Bedard <maxime.bedard@shopify.com>
2015-10-21 19:02:41 -0400

    Add SQL queries

---
+++ scripts/setup_db.sql
@@ -0,0 +1,57 @@
+create table users (
+  id serial PRIMARY KEY,
+  username varchar(100) NOT NULL UNIQUE,
+  pwd varchar(50) NOT NULL
+);
+
+create table user_roles (
+  user_id integer NOT NULL,
+  role_id integer NOT NULL,
+  UNIQUE (user_id, role_id)
+);
+
+create table roles (
+  id serial PRIMARY KEY,
+  role varchar(100) NOT NULL UNIQUE
+);
+
+create table dossiers (
+  id serial PRIMARY KEY,
+  nom varchar(255) NOT NULL,
+  prenom varchar(255) NOT NULL,
+  no_plaque varchar(255) NOT NULL,
+  no_permis varchar(255) NOT NULL
+);
+
+create table infractions (
+  id serial PRIMARY KEY,
+  description varchar(255) NOT NULL,
+  gravite integer NOT NULL
+);
+
+create table infraction_dossiers (
+  dossier_id integer NOT NULL,
+  infraction_id integer NOT NULL,
+  UNIQUE (dossier_id, infraction_id)
+);
+
+insert into users (username, pwd) VALUES
+  ('henry', 'bob12'),
+  ('gaston', 'toto13');
+
+
+insert into roles (role) VALUES
+  ('administrateur')
+  ('utilisateur');
+
+insert into user_roles (user_id, role_id) VALUES
+  (1,1),
+  (2,2);
+
+insert into dossiers (nom, prenom, no_plaque, no_permis) VALUES
+  ('henry', 'lemieux', '123456', '987654'),
+  ('gaston', 'rinfrette', '789789', '123456');
+
+insert into infractions (description, gravite) VALUES
+  ('Delit de fuite', 1);
+
